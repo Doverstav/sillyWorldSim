1063b62508d631794057a58cbf3e5df1081ccf33
Pontus Doverstav <doverstav@gmail.com>
2017-03-03 16:53:24 +0100

    Began work on mapObject

--- src/model/MapObject.java
+++ src/model/MapObject.java
@@ -4,4 +4,23 @@ package model;
  * Created by dover on 2017-03-03.
  */
 public abstract class MapObject {
+
+    private Position pos;
+    private Enum type;
+
+    public MapObject(Position p, Enum t){
+        pos = p;
+        type = t;
+    }
+
+    public Position getPosition(){
+        return pos;
+    }
+
+    public Enum getType(){
+        return type;
+    }
+
+    public void update(){};
+
 }
