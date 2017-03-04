ad30e04aa5d99d69ed2afcda557898ef6a511551
Pontus Doverstav <doverstav@gmail.com>
2017-03-04 13:05:53 +0100

    Added enum for roads

--- src/model/FieldType.java
+++ src/model/FieldType.java
@@ -4,5 +4,5 @@ package model;
  * Created by dover on 2017-03-04.
  */
 public enum FieldType {
-    FOREST, MOUNTAIN, WATER, FIELD, CASTLE, VILLAGE
+    FOREST, MOUNTAIN, WATER, FIELD, CASTLE, VILLAGE, ROAD
 }
