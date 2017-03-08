705a1544a5cde4ee99855701a30b3ded18a0a465
Pontus Doverstav <doverstav@gmail.com>
2017-03-09 00:18:50 +0100

    Changed size of test map

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello world!");
-        Map m = new Map(10,10);
+        Map m = new Map(10,30);
         System.out.println(m.toString());
     }
 }
